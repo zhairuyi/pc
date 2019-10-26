7ac2958cf29c28e175ecae984d70e73a11e4b45f
zhairuyi <1240917235@qq.com>
2019-10-26 10:43:40 +0800

    登录页面完成

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../views/Login'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  routes: [{
+    path: '/Login',
+    component: Login
+  }]
+})
---
+++ src/views/Login/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <el-card class="box-card">
+      <img src="../../assets/logo_index.png" alt />
+      <el-form :model="form">
+        <el-form-item>
+          <el-input v-model="form.phone" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.num" placeholder="请输入验证码" style="width:230px;margin-right:8px"></el-input>
+          <!-- </el-form-item>
+          <el-form-item>-->
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">您已阅读并同意本协议</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        phone: '',
+        num: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background-color: cadetblue;
+  background: url("../../assets/login_bg.jpg") no-repeat center / cover;
+  .el-card {
+    width: 400px;
+    height: 400px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      width: 200px;
+      display: block;
+      margin: 0 auto 30px;
+    }
+  }
+}
+</style>
